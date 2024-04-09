0df3b32c50ffe99de268a21f7a3ff302a8d45fa4
Davy <davy@trypavlov.com>
2024-04-10 00:45:25 +0100

    more content

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,132 @@
 import React, { useCallback, useState} from 'react';
 import { ReactComponent as BaseRabbitIcon } from './rabbit.svg';
+import inLovingMemory from './media/in-loving-memory.jpg';
 import juliette from './media/juliette.jpg';
+import toby from './media/toby.jpg';
+import poolTable from './media/poolTable.jpg';
+import spikeball from './media/spikeball.jpg';
+import thatsAChoppin from './media/thats-a-choppin.jpg';
+import washingUp from './media/washing-up.mov';
+import toilet from './media/toilet.jpg';
 import fashionShow from './media/fashionShow.mov';
+import ducks from './media/ducks.png';
 import evilBunny from './media/evil-bunny.jpg';
 import './App.css';
+
+interface PageData {
+  clue: string;
+  location: string;
+  expectedAnswer: string;
+  media: any;
+  mediaType: "image" | "video";
+  memory: string;
+}
 
 function cx(...classes: any[]) {
   return classes.filter(Boolean).join(" ");
 }
 
-const DATA = {
+
+const DATA: { [key: string]: PageData} = {
   'Page1': {
-    clue: 'Where Juliet gave her first live performance',
-    expectedAnswer: '6969',
-    media: juliette,
+    clue: "You'll soon be a visitor in the office, but I think you're familiar with the process when you arrive",
+    location: 'The sign in iPad at reception',
+    expectedAnswer: '0000',
+    media: spikeball,
     mediaType: 'image',
-    memory: "It was a great show, but the real show was the friends we made along the way",
+    memory: "Spikeball in floor 1 of GES",
   },
   'Page2': {
-    clue: 'Ready for a nap?',
+    clue: 'Where Juliet gave her first live performance',
+    location: "the lectern in woolamaloo",
+    expectedAnswer: '6969',
+    media: toilet,
+    mediaType: 'image',
+    memory: "It was a good performance, but Pranava got the real show in the ladies room afterwards",
+  },
+  'Page3': {
+    clue: "Do you believe...? You'll need something to sign with",
+    location: 'floor 9 stationary cupboard',
+    expectedAnswer: '8008',
+    media: toby,
+    mediaType: 'image',
+    memory: '...in love after love?',
+  },
+  'Page4': {
+    clue: "The final countdown... We're leaving forever...",
+    location: "underneath Davy's desk",
+    expectedAnswer: '4321',
+    media: juliette,
+    mediaType: 'image',
+    memory: '2024 EOY ROAST!',
+  },
+  'Page5': {
+    clue: 'Time to join Liz for a game of Splendor',
+    location: 'Marrickville',
+    expectedAnswer: 'ruby',
+    media: ducks,
+    mediaType: 'image',
+    memory: 'Run out of fucks? No problem, just sub in some ducks!',
+  },
+  'Page6': {
+    clue: "A spicy meeting is happening in redfern, let's listen in...",
+    location: 'The wellness room',
     expectedAnswer: '0420',
+    media: '',
+    mediaType: 'image',
+    memory: '',
+  },
+  'Page7': {
+    clue: "I'm board... let's play a game",
+    location: 'the board games shelf outside the kitchen',
+    expectedAnswer: '1001',
     media: fashionShow,
     mediaType: 'video',
-    memory: "It was a great show, but the real show was the friends we made along the way",
-    nextClue: "It's the final countdown, we're leaving forever!",
+    memory: 'And Amir never asked Adam for another favour, ever again.',
+  },
+  'Page8': {
+    clue: 'Not quite the corner pocket',
+    location: 'the pool table',
+    expectedAnswer: '8888',
+    media: poolTable,
+    mediaType: 'image',
+    memory: '',
+  },
+  'Page9': {
+    clue: 'Getting hungry? Time for a snack!',
+    location: 'the snack shelf',
+    expectedAnswer: 'nuts',
+    media: thatsAChoppin,
+    mediaType: 'image',
+    memory: '',
+  },
+  'Page10': {
+    clue: "We've gotten a bit dirty... time to clean up",
+    location: "Near the sink in the kitchen",
+    expectedAnswer: 'soap',
+    media: washingUp,
+    mediaType: 'video',
+    memory: "It's not that hard... IS WHAT SHE SAID!",
   },
 };
 
 function RabbitIcon(props: any) {
-  return <button onClick={props.onClick} disabled={!props.completed}>
-    <BaseRabbitIcon
-      style={{
-        stroke: 'black',
-        strokeWidth: '0.3pt',
-        fill: props.completed ? '#7fbe7f' : '#c5c7c5',
-      }}    
-      className="h-10 w-10"
-    />
-  </button>;
+  return (
+    <button 
+      onClick={props.onClick}
+      disabled={!props.completed}
+      className="border-solid border-1 shadow-md border-grey-50 rounded-full p-1"
+    >
+      <BaseRabbitIcon
+        style={{
+          stroke: 'black',
+          strokeWidth: '0.3pt',
+          fill: props.completed ? '#7fbe7f' : '#c5c7c5',
+        }}    
+        className="h-8 w-8"
+      />
+    </button>
+  );
 };
 
 function useStickyState(defaultValue: any, key: string) {
@@ -66,7 +155,6 @@ function AnswerBox({ onAnswer, showError }: { onAnswer: Function, showError: boo
       <form onSubmit={submitForm}>
         <input
           name="answer"
-          type="number"
           className={cx("border p-2 rounded-lg mt-8", showError ? "border-red-500": "border-gray-300" )}
           placeholder="Look under your rabbit"
         />
@@ -102,6 +190,7 @@ function NextClueButton({ nextPage }: { nextPage: Function }) {
 function Intro({ nextPage }: { nextPage: Function }) {
   return (
     <div className="flex flex-col items-center mt-8">
+      <img alt="In loving memory" src={inLovingMemory} className="w-64"></img>
       <h1 className="text-2xl font-bold mb-8">Welcome to the treasure hunt!</h1>
       <div className="flex flex-col items-center gap-4 text-base text-center">
         <p>We've hidden rabbits throughout the office.</p>
@@ -123,12 +212,12 @@ function DumbPage({ clue, media, mediaType, memory, answer, answered, onAnswer, 
             <img
               alt={clue}
               src={media}
-              className="h-64 w-64"
+              className="w-64"
             ></img>
           )}
           {mediaType ==='video' && (
-            <video className="h-64 w-64" controls autoPlay loop muted>
-            <source src={fashionShow} type="video/mp4"></source>
+            <video className="w-64" controls autoPlay loop muted>
+            <source src={media} type="video/mp4"></source>
           </video>
           )}
           <p className="text-center">{memory}</p>
@@ -145,26 +234,15 @@ function DumbPage({ clue, media, mediaType, memory, answer, answered, onAnswer, 
   );
 }
 
-function Page1({ nextPage, answer, onAnswer  }: any) {
-  const { clue, media, mediaType, memory, expectedAnswer } = DATA['Page1'];
-  const answered = answer === expectedAnswer;
-
-  return (
-    <DumbPage
-      clue={clue}
-      media={media}
-      mediaType={mediaType}
-      memory={memory}
-      answer={answer}
-      answered={answered}
-      onAnswer={onAnswer}
-      nextPage={nextPage}
-    />
-  );
+interface SmartPageProps {
+  currentPage: string;
+  nextPage: Function;
+  answer: string;
+  onAnswer: Function;
 }
 
-function Page2({ nextPage, answer, onAnswer }: any) {
-  const { clue, media, mediaType, memory, expectedAnswer } = DATA['Page2'];
+function SmartPage({ currentPage, nextPage, answer, onAnswer  }: SmartPageProps) {
+  const { clue, media, mediaType, memory, expectedAnswer } = DATA[currentPage];
   const answered = answer === expectedAnswer;
 
   return (
@@ -193,71 +271,86 @@ function LastPage() {
   );
 }
 
+function initialAnswersState(answered: string[] = []): {[key: string]: string} {
+  return {
+    'Page1': answered.includes('Page1') ? DATA['Page1'].expectedAnswer: '',
+    'Page2': answered.includes('Page2') ? DATA['Page2'].expectedAnswer: '',
+    'Page3': answered.includes('Page3') ? DATA['Page3'].expectedAnswer: '',
+    'Page4': answered.includes('Page4') ? DATA['Page4'].expectedAnswer: '',
+    'Page5': answered.includes('Page5') ? DATA['Page5'].expectedAnswer: '',
+    'Page6': answered.includes('Page6') ? DATA['Page6'].expectedAnswer: '',
+    'Page7': answered.includes('Page7') ? DATA['Page7'].expectedAnswer: '',
+    'Page8': answered.includes('Page8') ? DATA['Page8'].expectedAnswer: '',
+    'Page9': answered.includes('Page9') ? DATA['Page9'].expectedAnswer: '',
+    'Page10': answered.includes('Page10') ? DATA['Page10'].expectedAnswer: '',
+  };
+}
+
 function App() {
   const [answered, setAnswered] = useStickyState([], 'answered');
-  const [page1Answer, setPage1Answer] = useState(answered.includes('Page1') ? DATA['Page1'].expectedAnswer: '');
-  const [page2Answer, setPage2Answer] = useState(answered.includes('Page2') ? DATA['Page2'].expectedAnswer: '');
-  const [currentPage, setCurrentPage] = useState(0);
+  const [pageAnswers, setPageAnswers] = useState(initialAnswersState(answered));
+  const [currentPage, setCurrentPage] = useState('intro');
 
   const allAnswered = answered.includes('Page1') && 
     answered.includes('Page2');
+  
+  const pages = Array.from({ length: 10 }, (_, i) => `Page${i + 1}`);
+
 
   return (
-    <div className="flex flex-col items-center mt-16">
-      <div className="flex flex-row items-center gap-8">
-        <RabbitIcon
-          completed={answered.includes('Page1')}
-          onClick={() => {
-            setCurrentPage(1);
-          }}
-        />
-        <RabbitIcon
-          completed={answered.includes('Page2')}
-          onClick={() => {
-            setCurrentPage(2);
-          }}
-        />
+    <div className="flex flex-col items-center mt-16 ml-4 mr-4 gap-1">
+      <div className="flex flex-row flex-wrap items-center gap-2">
+        {pages.slice(0,6).map((page) => (
+          <RabbitIcon
+            completed={answered.includes(page)}
+            onClick={() => {
+              setCurrentPage(page);
+            }}
+          />
+        ))}
+      </div>
+      <div className="flex flex-row flex-wrap items-center gap-2">
+        {pages.slice(6).map((page) => (
+          <RabbitIcon
+            completed={answered.includes(page)}
+            onClick={() => {
+              setCurrentPage(page);
+            }}
+          />
+        ))}
         <RabbitIcon
           completed={allAnswered}
           onClick={() => {
-            setCurrentPage(3);
+            setCurrentPage('last');
           }}
         />
       </div>
-      { currentPage === 0 && (
+      { currentPage === 'intro' && (
         <Intro nextPage={() => {
-          setCurrentPage(1);
+          setCurrentPage('Page1');
         }} />
       )}
-      {currentPage === 1 && (
-        <Page1 
+      {pages.includes(currentPage) && (
+        <SmartPage
+          currentPage={currentPage}
           nextPage={() => {
-            setCurrentPage(2);
+            const currentPageIndex = pages.indexOf(currentPage);
+            if (currentPageIndex < pages.length - 1) {
+              setCurrentPage(pages[currentPageIndex + 1]);
+            } else {
+              setCurrentPage('last');
+            }
           }}
-          answer={page1Answer}
+          answer={pageAnswers[currentPage]}
           onAnswer={(answer: string) => {
-            if (answer === DATA['Page1'].expectedAnswer) {
-              setPage1Answer(answer);
-              setAnswered([...answered, 'Page1']);
+            setPageAnswers(Object.assign({}, pageAnswers, { [currentPage]: answer }));
+            if (answer === DATA[currentPage].expectedAnswer) {
+              setAnswered([...answered, currentPage]);
             }
           }}
         />
       )}
-      {currentPage === 2 && (
-        <Page2
-          nextPage={() => {
-            setCurrentPage(3);
-          }}
-          answer={page2Answer}
-          onAnswer={(answer: string) => {
-            if (answer === DATA['Page2'].expectedAnswer) {
-              setPage2Answer(answer);
-              setAnswered([...answered, 'Page2']);
-            }
-          }}
-        />
-      )}
-      {currentPage === 3 && (
+      {currentPage === 'last' && (
         <LastPage />
       )}
     </div>
